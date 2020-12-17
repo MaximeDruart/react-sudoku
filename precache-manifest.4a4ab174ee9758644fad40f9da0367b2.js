810d29c158157cd7d3b3f2236f57686ee9c8093f
Maxime Druart <maxime41010.druart@gmail.com>
2020-12-17 15:06:41 +0100

    Updates

---
+++ precache-manifest.4a4ab174ee9758644fad40f9da0367b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba70d562a0e751988d0941501736ded4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c8ef5a45032f56ad4b92",
+    "url": "./static/js/2.4d834d75.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.4d834d75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ad872d039b299dfb42",
+    "url": "./static/js/main.b0f0691c.chunk.js"
+  },
+  {
+    "revision": "30e7e582d0d9bc8bb9b7",
+    "url": "./static/js/runtime-main.fa59bc8b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c5ef65c2efe1bff8487f5ea1605eb119.js"
+  "./precache-manifest.4a4ab174ee9758644fad40f9da0367b2.js"
 );
 
 self.addEventListener('message', (event) => {
