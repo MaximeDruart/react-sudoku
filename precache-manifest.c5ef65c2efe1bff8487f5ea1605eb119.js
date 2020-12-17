0bc629211571e45ca4f5f73cc4671f6b04f80864
Maxime Druart <maxime41010.druart@gmail.com>
2020-12-17 12:58:18 +0100

    Updates

---
+++ precache-manifest.c5ef65c2efe1bff8487f5ea1605eb119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21fa0189a916f9eed92bbd622b4522f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c7a5e78d9e57857c26c",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "7a9ede76fccec95c06df",
+    "url": "./static/js/2.d3e6cacb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.d3e6cacb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c7a5e78d9e57857c26c",
+    "url": "./static/js/main.64c705e6.chunk.js"
+  },
+  {
+    "revision": "2700fec9f8dd9047f9b4",
+    "url": "./static/js/runtime-main.2777ef87.js"
+  }
+]);
